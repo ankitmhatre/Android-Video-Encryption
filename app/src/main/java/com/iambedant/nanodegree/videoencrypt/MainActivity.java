abf1208c1b18f314c32e489463ee0f0d883f85c2
Ankit <ankitmhatre.mhatre9@gmail.com>
2020-07-04 20:33:04 +0530

    Reading a Usb

--- app/src/main/java/com/iambedant/nanodegree/videoencrypt/MainActivity.java
+++ app/src/main/java/com/iambedant/nanodegree/videoencrypt/MainActivity.java
@@ -1,29 +1,56 @@
 package com.iambedant.nanodegree.videoencrypt;
 
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbEndpoint;
+import android.hardware.usb.UsbInterface;
+import android.hardware.usb.UsbManager;
+import android.hardware.usb.UsbRequest;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 import com.iambedant.xcript.Encrypter;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_TAKE_GALLERY_VIDEO = 11;
-    Encrypter encrypter ;
+    private static final String ACTION_USB_PERMISSION =
+            "com.android.example.USB_PERMISSION";
+    private static final String TAG = "UsbDevices";
 
+    private static final int REQUEST_TAKE_GALLERY_VIDEO = 11;
+    Encrypter encrypter;
+    PendingIntent permissionIntent;
+    UsbManager manager;
+
+
+    //connection variables
+
+    private static int TIMEOUT = 0;
+    private boolean forceClaim = true;
+    private Uri finalUri;
+    UsbDevice device;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,18 +77,86 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Uri storeUri = Uri.parse("file:///storage/emulated/0/DCIM/Camera/enc_v.swf");
-                File out = new File(outputPath);
-                if (out.exists()) {
-                    out.delete();
+                File file = new File(outputPath);
+
+                int size = (int) file.length();
+                byte[] bytes = new byte[size];
+                try {
+                    BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
+                    buf.read(bytes, 0, bytes.length);
+                    buf.close();
+                    Log.d(TAG, "buff");
+                    startTransfer(bytes);
+
+
+                    if (file.exists()) {
+                        file.delete();
+                    }
+                    encrypter.decryptFile(storeUri, outputPath, key);
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
                 }
-                encrypter.decryptFile(storeUri, outputPath, key);
+
+
             }
         });
 
+        manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        device = (UsbDevice) getIntent().getParcelableExtra(UsbManager.EXTRA_DEVICE);
+
+
+//        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+//        HashMap<String, UsbDevice> deviceList = manager.getDeviceList();
+//        Iterator it = deviceList.entrySet().iterator();
+//        while(it.hasNext()){
+//            Map.Entry pair = (Map.Entry)it.next();
+//            Log.d("UsbDevices", pair.getKey() + " = " + pair.getValue());
+//        }
+
+        permissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+        registerReceiver(usbReceiver, filter);
+    }
+
+    private void startTransfer(final byte[] bytes) {
+        if (device != null) {
+            Log.d(TAG, "device:" + device.getDeviceName());
+
+            if (manager.hasPermission(device)) {
+                Log.d(TAG, "permisiion:" + true);
+
+                UsbInterface intf = device.getInterface(0);
+                final UsbEndpoint endpoint = intf.getEndpoint(0);
+
+                final UsbDeviceConnection connection = manager.openDevice(device);
+                connection.claimInterface(intf, forceClaim);
+
+                final UsbRequest usbRequest = new UsbRequest();
+
+                usbRequest.initialize(connection, endpoint);
+                final ByteBuffer buf = ByteBuffer.wrap(bytes);
+                usbRequest.queue(buf, bytes.length);
+                usbRequest.setClientData(this);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d(TAG, "bulkTransfer has Started");
+
+                        connection.bulkTransfer(endpoint, bytes, bytes.length, TIMEOUT);
+                        Log.d(TAG, "bulkTransfer has ended" + bytes.length);
+                    }
+                }, 0);
+            }
+        }
     }
 
     private void init() throws NoSuchAlgorithmException {
-        encrypter =new Encrypter();
+        encrypter = new Encrypter();
     }
 
 
@@ -85,6 +180,7 @@ public class MainActivity extends AppCompatActivity {
                 Uri storeUri = Uri.parse("file:///storage/emulated/0/DCIM/Camera/enc_v.swf");
                 outputPath = mf_szGetRealPathFromURI(selectedUri);
                 key = encrypter.encryptFile(outputPath, storeUri);
+                Log.d(TAG, "encrypted");
             }
         }
     }
@@ -132,4 +228,26 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private final BroadcastReceiver usbReceiver = new BroadcastReceiver() {
+
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        if (device != null) {
+                            Log.d(TAG, "we have the permission " + device);
+                            //call method to set up device communication
+                            manager.requestPermission(device, permissionIntent);
+                        }
+                    } else {
+                        Log.d(TAG, "permission denied for device " + device);
+                    }
+                }
+            }
+        }
+    };
 }
